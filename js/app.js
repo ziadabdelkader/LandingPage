8a41781ef07c919454a136a6a8a362685b2071a8
ziadabdelkader <zyad012ziad012@gmail.com>
2021-03-21 14:14:58 +0200

    Add active state functionality and make all align left

--- js/app.js
+++ js/app.js
@@ -20,12 +20,42 @@
 
 const sections = document.querySelectorAll("section");
 const menuList = document.querySelector("#navbar__list");
+let activeSection = sections[0];
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+function isElementInViewport (element) {
+
+    let rect = element.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+function getActiveSection(){
+    for(const section of sections){
+        if(isElementInViewport(section))
+            return section;
+    }
+    return activeSection;
+}
+
+function scrollWindowHandler(){
+    console.log("scrollWindowHandler")
+    const newActiveSection = getActiveSection();
+    if(newActiveSection !== activeSection){
+        activeSection.classList.remove("your-active-class")
+        newActiveSection.classList.add("your-active-class")
+        activeSection = newActiveSection
+    }
+}
 
 function addNavigationMenuItems(){
     for(const section of sections){
@@ -47,7 +77,7 @@ function handlerForNavigationItemClicked(event){
     const navigationItem = event.target;
     const sectionId = navigationItem.dataset.sectionId;
     const section = document.getElementById(sectionId);
-    section.scrollIntoView();
+    section.scrollIntoView({behavior: "smooth",block: 'start'});
 }
 
 
@@ -78,6 +108,6 @@ addNavigationMenuItems();
 
 // Scroll to section on link click
 menuList.addEventListener('click',handlerForNavigationItemClicked);
-
+document.addEventListener("scroll",scrollWindowHandler)
 
 // Set sections as active
